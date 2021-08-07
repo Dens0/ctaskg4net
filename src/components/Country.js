a23f5043835686c13783725b24005ba55443a483
Dens0 <adrian.sutkowski@gmail.com>
2021-08-08 00:56:10 +0200

    added meta for mobile

--- src/components/Country.js
+++ src/components/Country.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import classes from './Country.module.css'
 import CountryPopup from './CountryPopup'
 // import {CSSTransition} from 'react-transition-group';
+import Popup from 'react-animated-popup'
 
 const Country = (props) => {
 
@@ -17,15 +18,9 @@ const Country = (props) => {
     };
     return (<>
             <li onClick={popupHandle} className={classes.country__name}>{props.name}</li>
-            {/*<CSSTransition*/}
-            {/*    in={popup}*/}
-            {/*    timeout={300}*/}
-            {/*    classNames="alert"*/}
-            {/*    unmountOnExit*/}
-            {/*>*/}
+
                 {popup === true ?
                     <CountryPopup onArrow={exitHandle} showPopup={popup} data={props.countryData}> {props.name}</CountryPopup> : ''}
-            {/*</CSSTransition>*/}
         </>
     )
 };
